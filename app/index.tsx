6cce5b8182f6b4a8b3d2bd6b675e08564a6b69b4
Daisy <daisybutler16@gmail.com>
2024-12-19 21:16:34 +0000

    Basic components.

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,31 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { ActivityIndicator, Button, StyleSheet, TextInput, View } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonContainer: {
+    backgroundColor: 'purple',
+    borderRadius: 10,
+    padding: 10,
+    color: 'white'
+  },
+});
 
 export default function Index() {
+  const [isAnimating, setIsAnimating] = useState(false);
+  const [email, onEnterEmail] = useState('Email');
+  const [password, onEnterPassword] = useState('Password');
+
+  function handlePress() {
+    setIsAnimating(prev => !prev)
+    console.log(email)
+    console.log(password)
+  }
+
   return (
     <View
       style={{
@@ -9,7 +34,12 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <TextInput value={email} onChangeText={onEnterEmail} />
+      <TextInput value={password} onChangeText={onEnterPassword} />
+      <View style={styles.buttonContainer}>
+        <Button color='white' title='Login' onPress={handlePress} />
+      </View>
+      <ActivityIndicator animating={isAnimating} />
     </View>
   );
 }
